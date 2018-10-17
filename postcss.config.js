6a9b0304bf00ebc6f5b8d513099cf8bd1d5d11ab
sosout <sosout@139.com>
2018-10-17 11:25:01 +0800

    fix(postcss.config.js): browsers
    browsers

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,12 @@ module.exports = {
   plugins: {
     'postcss-import': {},
     'precss': {},
-    'postcss-cssnext': {},
-    'postcss-calc': {}
+    'autoprefixer': {
+      browsers: [
+        '> 1%',
+        'last 2 versions',
+        'not ie <= 10'
+      ]
+    }
   }
 };
